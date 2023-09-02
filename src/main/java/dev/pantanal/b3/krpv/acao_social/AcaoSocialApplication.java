b33bf7b574899e554aad0cae242f51bd174b4c4c
Kaio Lino <kaiomudkt@gmail.com>
2023-09-02 11:39:47 -0400

    iniciado projeto

---
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/AcaoSocialApplication.java
@@ -0,0 +1,13 @@
+package dev.pantanal.b3.krpv.acao_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaoSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaoSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pantanal/b3/krpv/acao_social/AcaoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pantanal.b3.krpv.acao_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaoSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
