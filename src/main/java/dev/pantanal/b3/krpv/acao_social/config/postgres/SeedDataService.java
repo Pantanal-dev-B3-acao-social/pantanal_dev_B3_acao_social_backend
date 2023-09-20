144c3f169bcc4957e795f29aaf173af3a082a388
Kaio Lino <kaiomudkt@gmail.com>
2023-09-20 14:33:56 -0400

    preparando função que busca registro aleatorio

--- src/main/java/dev/pantanal/b3/krpv/acao_social/config/postgres/SeedDataService.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/config/postgres/SeedDataService.java
@@ -4,15 +4,9 @@ import dev.pantanal.b3.krpv.acao_social.config.postgres.factory.SocialActionFact
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.util.FileCopyUtils;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class SeedDataService {
-
 
     private final SocialActionFactory socialActionFactory;
 
--- src/main/java/dev/pantanal/b3/krpv/acao_social/config/postgres/factory/SocialActionFactory.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/config/postgres/factory/SocialActionFactory.java
@@ -2,6 +2,7 @@ package dev.pantanal.b3.krpv.acao_social.config.postgres.factory;
 import com.github.javafaker.Faker;
 import dev.pantanal.b3.krpv.acao_social.modulos.socialAction.dto.SocialActionDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.Random;
@@ -21,7 +22,22 @@ public class SocialActionFactory {
             UUID.randomUUID(),
             faker.name().fullName(),
             faker.lorem().sentence()
+                // string ongId = findOneRandom("ong");
+                // string levelId = findOneRandom("category_project_level");
+                // string typeId = findOneRandom("category_project_type");
         );
+    }
+
+    public String findOneRandom(String table) {
+        String sql = "SELECT * FROM "+ table +" ORDER BY RANDOM() LIMIT 1";
+        try {
+            String id = jdbcTemplate.queryForObject(sql, String.class);
+            return id;
+        } catch (EmptyResultDataAccessException e) {
+            // Trate o caso em que a tabela está vazia
+            // TODO: lançar exceção
+            return null;
+        }
     }
 
     public void insertOne(SocialActionDto socialActionDto) {
