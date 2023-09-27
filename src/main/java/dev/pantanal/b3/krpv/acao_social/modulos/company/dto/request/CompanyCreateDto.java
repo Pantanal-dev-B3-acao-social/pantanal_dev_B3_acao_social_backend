55c2a169af1fa321ed823f40b18680072ddd34e8
RobertoFORTs <bobfortesneto@gmail.com>
2023-09-27 14:23:34 -0400

    feat: add dtos for creating company request and response

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/company/dto/request/CompanyCreateDto.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/company/dto/request/CompanyCreateDto.java
@@ -1,5 +1,14 @@
 package dev.pantanal.b3.krpv.acao_social.modulos.company.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CompanyCreateDto (
+
+        @NotBlank(message="Campo 'name' não pode estar vazio")
+        String name,
+        @NotBlank(message= "Campo 'description' não pode estar vazio")
+        String description,
+        @NotBlank(message= "Campo 'cnpj' não podes estar vazio")
+        String cnpj
 
 ) {}
