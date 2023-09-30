c1ecd317c810acbe0f425af1ce6725c3e411f4ed
RobertoFORTs <bobfortesneto@gmail.com>
2023-09-30 19:38:06 -0400

    feat(investment): complete endpoint to create a Investment

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/InvestmentService.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/InvestmentService.java
@@ -1,9 +1,24 @@
 package dev.pantanal.b3.krpv.acao_social.modulos.Investment;
 
 import dev.pantanal.b3.krpv.acao_social.modulos.Investment.dto.request.InvestmentCreateDto;
+import dev.pantanal.b3.krpv.acao_social.modulos.Investment.repository.InvestmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvestmentService {
-    public InvestmentEntity create(InvestmentCreateDto request) {
 
+    @Autowired
+    private InvestmentRepository repository;
+
+    public InvestmentEntity create(InvestmentCreateDto request) {
+        InvestmentEntity newInvestiment = new InvestmentEntity();
+        newInvestiment.setValue_money(request.value_money());
+        newInvestiment.setDate(request.date());
+        newInvestiment.setMotivation(request.motivation());
+        newInvestiment.setApprovedAt(request.approvedAt());
+        InvestmentEntity savedInvestment = repository.save(newInvestiment);
+        return savedInvestment;
     }
+
 }
--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/repository/InvestmentRepository.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/repository/InvestmentRepository.java
@@ -2,9 +2,23 @@ package dev.pantanal.b3.krpv.acao_social.modulos.Investment.repository;
 
 import dev.pantanal.b3.krpv.acao_social.modulos.Investment.InvestmentEntity;
 import dev.pantanal.b3.krpv.acao_social.modulos.category.CategoryEntity;
-
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+@Component
+@RequiredArgsConstructor
+@Repository
 public class InvestmentRepository {
-    public InvestmentEntity save(InvestmentEntity toSave) {
 
+    @Autowired
+    private InvestmentPostgresRepository investmentPostgresRepository;
+    @PersistenceContext
+    private final EntityManager entityManager;
+    public InvestmentEntity save(InvestmentEntity entityObj) {
+        InvestmentEntity investmentEntity = investmentPostgresRepository.save(entityObj);
+        return investmentEntity;
     }
 }
