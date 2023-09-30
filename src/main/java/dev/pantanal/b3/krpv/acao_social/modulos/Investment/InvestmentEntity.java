587b40697d8c0ca38549d0aa8b49faf5b8b1d48d
RobertoFORTs <bobfortesneto@gmail.com>
2023-09-30 18:26:53 -0400

    feat: add Investment Entity

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/InvestmentEntity.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/Investment/InvestmentEntity.java
@@ -1,5 +1,113 @@
 package dev.pantanal.b3.krpv.acao_social.modulos.Investment;
 
+import dev.pantanal.b3.krpv.acao_social.config.audit.AuditListener;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name="investment")
+@Entity(name="Investiment")
+@AuditTable("z_aud_investment")
+@EntityListeners(AuditListener.class)
+@Audited
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
 public class InvestmentEntity {
+
+    @Valid
+    @Version
+    private Long version;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid")
+    @NotNull
+    UUID id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private long value_money;
+
+    @Column(nullable = false)
+    @NotBlank
+    @PastOrPresent
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String motivation;
+
+//    @ManyToOne
+//    @JoinColumn("manager_id")
+//    private Person person;
+
+    @Column(nullable = false)
+    @NotBlank
+    private LocalDateTime aprovedAt;
+
+    @CreatedBy
+    private UUID createdBy;
+
+    @LastModifiedBy
+    private UUID lastModifiedBy;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name = "deleted_date")
+    private LocalDateTime deletedDate;
+
+    @Column(name = "deleted_by")
+    private UUID deletedBy;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String userId = authentication.getName();
+            this.createdBy = UUID.fromString(userId);
+        }
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastModifiedDate = LocalDateTime.now();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String userId = authentication.getName();
+            this.lastModifiedBy = UUID.fromString(userId);
+        }
+    }
+
+    @PreRemove
+    protected void onRemove() {
+        this.deletedDate = LocalDateTime.now();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String userId = authentication.getName();
+            this.deletedBy = UUID.fromString(userId);
+        }
+    }
 }
