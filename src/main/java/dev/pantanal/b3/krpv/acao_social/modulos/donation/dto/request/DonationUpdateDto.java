bc05026e4cb32a4bb6c37a60361ceee3230a4437
Pedrao1234 <112420528+Pedrao1234@users.noreply.github.com>
2023-10-03 21:52:37 -0400

    adicionando esqueleto para crud de donation

---
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/donation/dto/request/DonationUpdateDto.java
@@ -0,0 +1,4 @@
+package dev.pantanal.b3.krpv.acao_social.modulos.donation.dto.request;
+
+public class DonationUpdateDto {
+}
---
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/donation/dto/response/DonationResponseDto.java
@@ -0,0 +1,4 @@
+package dev.pantanal.b3.krpv.acao_social.modulos.donation.dto.response;
+
+public class DonationResponseDto {
+}
