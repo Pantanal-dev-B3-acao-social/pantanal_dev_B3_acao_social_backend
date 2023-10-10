6650fd22bac1e9fdb7fd254c066b10e6e36d5c33
Kaio Lino <kaiomudkt@gmail.com>
2023-10-10 13:07:05 -0400

    test: implementando testes PresenceControllerIT.java

---
+++ src/test/java/dev/pantanal/b3/krpv/acao_social/modules/presence/PresenceControllerIT.java
@@ -0,0 +1,288 @@
+package dev.pantanal.b3.krpv.acao_social.modules.presence;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import dev.pantanal.b3.krpv.acao_social.config.postgres.factory.*;
+import dev.pantanal.b3.krpv.acao_social.modulos.auth.dto.LoginUserDto;
+import dev.pantanal.b3.krpv.acao_social.modulos.person.PersonEntity;
+import dev.pantanal.b3.krpv.acao_social.modulos.presence.PresenceEntity;
+import dev.pantanal.b3.krpv.acao_social.modulos.presence.repository.PresenceRepository;
+import dev.pantanal.b3.krpv.acao_social.modulos.session.SessionEntity;
+import dev.pantanal.b3.krpv.acao_social.modulos.socialAction.SocialActionEntity;
+import dev.pantanal.b3.krpv.acao_social.utils.FindRegisterRandom;
+import dev.pantanal.b3.krpv.acao_social.utils.GenerateTokenUserForLogged;
+import dev.pantanal.b3.krpv.acao_social.utils.LoginMock;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import static dev.pantanal.b3.krpv.acao_social.modulos.presence.PresenceController.ROUTE_PRESENCE;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
+public class PresenceControllerIT {
+
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    GenerateTokenUserForLogged generateTokenUserForLogged;
+    @Autowired
+    LoginMock loginMock;
+    private DateTimeFormatter formatter;
+    ObjectMapper objectMapper;
+    @Autowired
+    MockMvc mockMvc;
+    private String tokenUserLogged;
+    @Autowired
+    private PresenceFactory presenceFactory;
+    @Autowired
+    private PresenceRepository presenceRepository;
+    @Autowired
+    private CategoryFactory categoryFactory;
+    @Autowired
+    private SocialActionFactory socialActionFactory;
+    @Autowired
+    CategoryGroupFactory categoryGroupFactory;
+    @Autowired
+    PersonFactory personFactory;
+    @Autowired
+    SessionFactory sessionFactory;
+    List<UUID> usersIds = new ArrayList<>();
+    List<SocialActionEntity> socialActionEntities;
+    List<PersonEntity> personEntities;
+    List<SessionEntity> sessionsEntities;
+    private static final Random random = new Random();
+
+    @BeforeEach
+    public void setup() throws Exception {
+        tokenUserLogged = generateTokenUserForLogged.loginUserMock(new LoginUserDto("funcionario1", "123"));
+        loginMock.authenticateWithToken(tokenUserLogged);
+        // formatar data hora
+        this.formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME; // Formate o LocalDateTime esperado
+        // mapper
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule());
+        // cadastra session
+        this.sessionsEntities = this.sessionFactory.insertMany(100);
+        // cadastrar persons
+        List<UUID> usersRandom = IntStream.range(0, 30)
+                .mapToObj(i -> UUID.randomUUID())
+                .collect(Collectors.toList());
+        this.personEntities = this.personFactory.insertMany(usersRandom.size(), usersRandom);
+    }
+
+    @Test
+    @DisplayName("lista paginada de presence com sucesso")
+    void findAllPresence() throws Exception {
+        // Arrange (Organizar)
+        List<PresenceEntity> saved = presenceFactory.insertMany(4, personEntities, sessionsEntities, personEntities);
+        // Act (ação)
+        ResultActions perform = mockMvc.perform(
+                MockMvcRequestBuilders.get(ROUTE_PRESENCE)
+                .header("Authorization", "Bearer " + tokenUserLogged)
+        );
+        // Assert (Verificar)
+        perform
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content", hasSize(4)));
+        int i = 0;
+        for (PresenceEntity item : saved) {
+            perform
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].id").value(item.getId().toString()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].person.id").value(item.getPerson().getId().toString()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].session.id").value(item.getSession().getId().toString()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].approvedBy.id").value(item.getApprovedBy().getId().toString()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].approvedDate").value(item.getApprovedDate().format(formatter)))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].createdDate").isNotEmpty())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].createdBy").value(item.getCreatedBy().toString()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].createdDate").value(item.getCreatedDate().format(formatter)))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].lastModifiedBy").value(
+                            item.getLastModifiedBy() == null  ?
+                                    null : item.getLastModifiedBy().toString())
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].lastModifiedDate").value(
+                            item.getLastModifiedDate() == null ?
+                                    null : item.getLastModifiedDate().format(formatter))
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].deletedDate").isEmpty())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.content[" + i + "].deletedBy").isEmpty());
+            i++;
+        }
+    }
+
+
+    @Test
+    @DisplayName("salva uma nova presence com sucesso")
+    void saveOnePresence() throws Exception {
+        // Arrange (Organizar)
+        Integer indexPerson = random.nextInt(0, personEntities.size());
+        Integer indexSession = random.nextInt(0, sessionsEntities.size());
+        PresenceEntity item = presenceFactory.makeFakeEntity(
+                personEntities.get(indexPerson),
+                sessionsEntities.get(indexSession),
+                personEntities.get(indexPerson)
+        );
+        String jsonRequest = objectMapper.writeValueAsString(item);
+        // Act (ação)
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(ROUTE_PRESENCE)
+                    .header("Authorization", "Bearer " + tokenUserLogged)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+        );
+        // Assert (Verificar)
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.session.id").value(item.getSession().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.person.id").value(item.getPerson().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedBy.id").value(item.getApprovedBy().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedDate").value(item.getApprovedDate().format(formatter)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdBy").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedBy").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdDate").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedDate").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedDate").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedBy").isEmpty());
+//        testar groupCategory
+    }
+
+
+    @Test
+    @DisplayName("Busca presence por ID com sucesso")
+    void findByIdPresence() throws Exception {
+        // Arrange (Organizar)
+        List<PresenceEntity> saved = presenceFactory.insertMany(3, personEntities, sessionsEntities, personEntities);
+        PresenceEntity item = saved.get(0);
+        // Act (ação)
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(ROUTE_PRESENCE + "/{id}", item.getId().toString())
+                        .header("Authorization", "Bearer " + tokenUserLogged)
+        );
+        // Assert (Verificar)
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(item.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.session.id").value(item.getSession().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.person.id").value(item.getPerson().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedBy.id").value(item.getApprovedBy().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedDate").value(item.getApprovedDate().format(formatter)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdBy").value(item.getCreatedBy().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdDate").value(item.getCreatedDate().format(formatter)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedDate").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedBy").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedBy").value(
+                        item.getLastModifiedBy() == null  ?
+                                null : item.getLastModifiedBy().toString())
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedDate").value(
+                        item.getLastModifiedDate() == null ?
+                                null : item.getLastModifiedDate().format(formatter))
+                );
+    }
+
+    @Test
+    @DisplayName("(hard-delete) Exclui uma presence com sucesso")
+    void deletePresence() throws Exception {
+        // Arrange (Organizar)
+        Integer indexPerson = random.nextInt(0, personEntities.size());
+        Integer indexSession = random.nextInt(0, sessionsEntities.size());
+        PresenceEntity item = presenceFactory.makeFakeEntity(
+                personEntities.get(indexPerson),
+                sessionsEntities.get(indexSession),
+                personEntities.get(indexPerson)
+        );
+        PresenceEntity savedItem = presenceFactory.insertOne(item);
+        // Act (ação)
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete(ROUTE_PRESENCE + "/{id}", savedItem.getId())
+                        .header("Authorization", "Bearer " + tokenUserLogged)
+        );
+        // Assert (Verificar)
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andDo(MockMvcResultHandlers.print());
+        PresenceEntity deleted = presenceRepository.findById(savedItem.getId());
+        Assertions.assertNull(deleted); // Deve ser nulo para passar o teste
+
+    }
+    // TODO: implementar soft-delete
+
+    @Test
+    @DisplayName("Atualiza uma presence com sucesso")
+    void updatePresence() throws Exception {
+        // Arrange (Organizar)
+        Integer indexPerson = random.nextInt(0, personEntities.size());
+        Integer indexSession = random.nextInt(0, sessionsEntities.size());
+        PresenceEntity item = presenceFactory.makeFakeEntity(
+                personEntities.get(indexPerson),
+                sessionsEntities.get(indexSession),
+                personEntities.get(indexPerson)
+        );
+        // Modifica alguns dados da presence
+        LocalDateTime approvedDateUpdate = item.getApprovedDate().plusHours(2).plusMinutes(40);
+        FindRegisterRandom<PersonEntity> findPersonRandom = new FindRegisterRandom<PersonEntity>(entityManager);
+        List<PersonEntity> persons = findPersonRandom.execute("person", 2, PersonEntity.class);
+        Integer indexSessionUpdate = random.nextInt(0, sessionsEntities.size());
+        item.setSession(sessionsEntities.get(indexSessionUpdate));
+        item.setPerson(persons.get(1));
+        item.setApprovedDate(approvedDateUpdate);
+        item.setApprovedBy(persons.get(0));
+        String updatedPresenceJson = objectMapper.writeValueAsString(item);
+        // Act (ação)
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.patch(ROUTE_PRESENCE + "/{id}", item.getId())
+                        .header("Authorization", "Bearer " + tokenUserLogged)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updatedPresenceJson)
+        );
+        // Assert (Verificar)
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(item.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.session.id").value(item.getSession().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.person.id").value(item.getPerson().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedBy.id").value(item.getApprovedBy().getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvedDate").value(item.getApprovedDate().format(formatter)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdBy").value(item.getCreatedBy().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdDate").value(item.getCreatedDate().format(formatter)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedDate").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deletedBy").isEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedBy").value(
+                        item.getLastModifiedBy() == null  ?
+                                null : item.getLastModifiedBy().toString())
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedDate").value(
+                        item.getLastModifiedDate() == null ?
+                                null : item.getLastModifiedDate().format(formatter))
+                );
+    }
+
+}
