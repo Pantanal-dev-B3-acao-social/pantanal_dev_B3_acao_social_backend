c2472eeb9e2f6e064b2356cda6eaae5251aa2019
Kaio Lino <kaiomudkt@gmail.com>
2023-10-10 21:05:01 -0400

    fix: correção CORS

---
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/config/web/CorsConfig.java
@@ -0,0 +1,27 @@
+package dev.pantanal.b3.krpv.acao_social.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${acao-social.frontend.baseUrl}")
+    private String urlFrontend;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(urlFrontend); // Domínio do seu frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
