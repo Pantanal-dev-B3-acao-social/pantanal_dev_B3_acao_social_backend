73d20ec4d8d027425efa12bc8d68df64eebb40db
RobertoFORTs <bobfortesneto@gmail.com>
2023-10-22 11:40:52 -0400

    fix(voluntary): create endpoint fix

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/voluntary/dto/request/VoluntaryCreateDto.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/voluntary/dto/request/VoluntaryCreateDto.java
@@ -3,16 +3,21 @@ package dev.pantanal.b3.krpv.acao_social.modulos.voluntary.dto.request;
 import dev.pantanal.b3.krpv.acao_social.modulos.person.PersonEntity;
 import dev.pantanal.b3.krpv.acao_social.modulos.socialAction.SocialActionEntity;
 import dev.pantanal.b3.krpv.acao_social.modulos.voluntary.enums.StatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record VoluntaryCreateDto (
         String observation,
-        SocialActionEntity socialAction,
-        PersonEntity person,
+        @NotNull
+        UUID socialAction,
+        @NotNull
+        UUID person,
         StatusEnum status,
-        PersonEntity approvedBy,
+        @NotNull
+        UUID approvedBy,
         LocalDateTime approvedDate,
         Integer feedbackScoreVoluntary,
         String feedbackVoluntary
