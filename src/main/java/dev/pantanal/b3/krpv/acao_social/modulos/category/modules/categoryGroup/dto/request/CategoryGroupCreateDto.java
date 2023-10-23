825f892cbe0b5e8c25bd20dff3e98e3a740c51d8
RobertoFORTs <bobfortesneto@gmail.com>
2023-10-23 00:03:41 -0400

    fix(categoryGroup): patch and post fixed

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/category/modules/categoryGroup/dto/request/CategoryGroupCreateDto.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/category/modules/categoryGroup/dto/request/CategoryGroupCreateDto.java
@@ -5,12 +5,15 @@ import dev.pantanal.b3.krpv.acao_social.modulos.category.modules.categoryGroup.e
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record CategoryGroupCreateDto(
         @NotBlank(message="Campo 'name' não pode estar vazio")
         String name,
         @NotBlank(message= "Campo 'description' não pode estar vazio")
         String description,
-        CategoryGroupEntity parentCategoryGroupEntity,
+        @NotNull
+        UUID parentCategoryGroupEntity,
         @NotNull(message= "Campo 'visibility' não pode estar vazio")
         VisibilityCategoryGroupEnum visibility
 ) {
