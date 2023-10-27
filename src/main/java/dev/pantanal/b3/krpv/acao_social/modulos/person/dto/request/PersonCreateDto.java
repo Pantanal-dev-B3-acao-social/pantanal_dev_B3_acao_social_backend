9a4ef521903c75fa049518b6136c511f82837408
Kaio Lino <kaiomudkt@gmail.com>
2023-10-26 21:31:35 -0400

    feat: keyclockUser junto com person

--- src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/person/dto/request/PersonCreateDto.java
+++ src/main/java/dev/pantanal/b3/krpv/acao_social/modulos/person/dto/request/PersonCreateDto.java
@@ -1,14 +1,15 @@
 package dev.pantanal.b3.krpv.acao_social.modulos.person.dto.request;
 
 import dev.pantanal.b3.krpv.acao_social.modulos.person.enums.StatusEnum;
+import dev.pantanal.b3.krpv.acao_social.modulos.user.KeycloakUser;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record PersonCreateDto (
-        UUID userId,
         String name,
         LocalDateTime dateBirth,
         StatusEnum status,
-        String cpf
+        String cpf,
+        KeycloakUser keycloakUser
 ) {}
